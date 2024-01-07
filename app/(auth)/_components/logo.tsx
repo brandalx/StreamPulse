94ea81891cedf18ab0c830bebcb044ffa880a602
brndalx <113251192+brandalx@users.noreply.github.com>
2024-01-07 16:00:05 -0500

    update: logo layout for auth

--- app/(auth)/_components/logo.tsx
+++ app/(auth)/_components/logo.tsx
@@ -7,5 +7,15 @@ const font = Poppins({
 });
 
 export const Logo = () => {
-  return <div className="">Logo</div>;
+  return (
+    <div className="flex flex-col items-center gap-y-4 ">
+      <div className="bg-white rounded-full p-1">
+        <Image alt="StreamPulse" height="80" width="80" src="/spooky.svg" />
+      </div>
+      <div className={cn("flex flex-col items-center ", font.className)}>
+        <p className="text-xl font-semibold">StreamPulse</p>
+        <p className="text-sm text-muted-foreground">Let&apos;s Play!</p>
+      </div>
+    </div>
+  );
 };
