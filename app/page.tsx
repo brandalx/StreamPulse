c189530fb5fcf852d155114101e95e74f5c65dad
brndalx <113251192+brandalx@users.noreply.github.com>
2024-01-07 15:38:08 -0500

    feat: layout for auth, user button for dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
+"use client";
+import { UserButton } from "@clerk/clerk-react";
 import Image from "next/image";
 
 export default function Page() {
-  return <p>Only authenticated users can see this</p>;
+  return (
+    <div className="flex flex-col gap-y-4">
+      <h1>Dashboard</h1>
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
 }
