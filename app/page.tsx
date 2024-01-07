7f71f381aa1c092676d1534a0a30d43649e5df84
brndalx <113251192+brandalx@users.noreply.github.com>
2024-01-07 15:05:27 -0500

    feat: clerk auth sign up and login init

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,5 @@
-import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Page() {
-  return (
-    <Button size="lg" variant="destructive">
-      Stream Pulse
-    </Button>
-  );
+  return <p>Only authenticated users can see this</p>;
 }
