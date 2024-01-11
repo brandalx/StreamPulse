49420de0b564deb6ada75e5438dd519c5737049d
brndalx <113251192+brandalx@users.noreply.github.com>
2024-01-11 18:30:30 -0500

    feat: submiting search bar form

--- app/(browse)/_components/navbar/search.tsx
+++ app/(browse)/_components/navbar/search.tsx
@@ -9,9 +9,32 @@ import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 
 const Search = () => {
+  const router = useRouter();
+
+  const [value, setValue] = useState("");
+
+  const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!value) return;
+
+    const url = qs.stringifyUrl(
+      {
+        url: "/",
+        query: { term: value },
+      },
+      { skipEmptyString: true }
+    );
+
+    router.push(url);
+  };
   return (
-    <form className="relative w-full lg:w-[400px] flex items-center ">
+    <form
+      onSubmit={onSubmit}
+      className="relative w-full lg:w-[400px] flex items-center "
+    >
       <Input
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
         placeholder="Search"
         className="rounded-r-none focus-visible:ring-0 focus-visible:ring-transparent focus-visible:ring-offset-0"
       />
