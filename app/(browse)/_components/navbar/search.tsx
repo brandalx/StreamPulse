498199542ae23f82b44c5d10de650e0a12c79bf6
brndalx <113251192+brandalx@users.noreply.github.com>
2024-01-11 18:26:48 -0500

    feat: search input initial ui

--- app/(browse)/_components/navbar/search.tsx
+++ app/(browse)/_components/navbar/search.tsx
@@ -1,9 +1,31 @@
 "use client";
-
+import qs from "query-string";
+import { useState } from "react";
+import { SearchIcon, X } from "lucide-react";
 import React from "react";
 
+import { useRouter } from "next/navigation";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
 const Search = () => {
-  return <div> Search</div>;
+  return (
+    <form className="relative w-full lg:w-[400px] flex items-center ">
+      <Input
+        placeholder="Search"
+        className="rounded-r-none focus-visible:ring-0 focus-visible:ring-transparent focus-visible:ring-offset-0"
+      />
+
+      <Button
+        type="submit"
+        size="sm"
+        variant="secondary"
+        className="rounded-l-none"
+      >
+        <SearchIcon className="h-5 w-5 text-muted-foreground" />
+      </Button>
+    </form>
+  );
 };
 
 export default Search;
