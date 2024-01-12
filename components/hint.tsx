6277bf475af2faabca61e139fb600f9ec2ff86e2
brndalx <113251192+brandalx@users.noreply.github.com>
2024-01-12 18:30:57 -0500

    feat :hint comp init, interface

---
+++ components/hint.tsx
@@ -0,0 +1,20 @@
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import React from "react";
+
+interface HintProps {
+  label: string;
+  children: React.ReactNode;
+  asChild?: boolean;
+  side?: "top" | "bottom" | "left" | "right";
+  align?: "start" | "center" | "end";
+}
+const Hint = () => {
+  return <div>Hint</div>;
+};
+
+export default Hint;
