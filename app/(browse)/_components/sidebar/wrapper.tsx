585eab0ebae4da7914f5d7a2cf549e7f2b070554
brndalx <113251192+brandalx@users.noreply.github.com>
2024-01-12 18:16:50 -0500

    feat: usage of usesidebar hook in wrapper

--- app/(browse)/_components/sidebar/wrapper.tsx
+++ app/(browse)/_components/sidebar/wrapper.tsx
@@ -1,11 +1,20 @@
+"use client";
 interface WrapperProps {
   children: React.ReactNode;
 }
+import { useSidebar } from "@/store/use-sidebar";
 import React from "react";
+import { cn } from "@/lib/utils";
 
 const Wrapper = ({ children }: WrapperProps) => {
+  const { collapsed } = useSidebar((state) => state);
   return (
-    <aside className="fixed left-0 flex flex-col w-60 h-full bg-background border-r border-[#2D2E35] z-50">
+    <aside
+      className={cn(
+        "fixed left-0 flex flex-col w-60 h-full bg-background border-r border-[#2D2E35] z-50",
+        collapsed && "w-[70px]"
+      )}
+    >
       {children}
     </aside>
   );
