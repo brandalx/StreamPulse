419bf6fe96bfb4b84a7f13f1c3fec1406e1be335
brndalx <113251192+brandalx@users.noreply.github.com>
2024-01-12 18:17:24 -0500

    feat: toggle init

--- app/(browse)/_components/sidebar/index.tsx
+++ app/(browse)/_components/sidebar/index.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import Wrapper from "./wrapper";
+import Toggle from "./toggle";
 
 const Sidebar = () => {
-  return <Wrapper></Wrapper>;
+  return (
+    <Wrapper>
+      <Toggle />
+    </Wrapper>
+  );
 };
 
 export default Sidebar;
