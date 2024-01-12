aaf8be04e65d807a185a2b098229fab340c8080b
brndalx <113251192+brandalx@users.noreply.github.com>
2024-01-12 18:23:29 -0500

    feat: onCollapse toggle

--- app/(browse)/_components/sidebar/toggle.tsx
+++ app/(browse)/_components/sidebar/toggle.tsx
@@ -1,7 +1,29 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { useSidebar } from "@/store/use-sidebar";
+import { ArrowLeftFromLine } from "lucide-react";
 import React from "react";
 
 const Toggle = () => {
-  return <div>Toggle</div>;
+  const { collapsed, onExpand, onCollapse } = useSidebar((state) => state);
+
+  const label = collapsed ? "Expand" : "Collapse";
+  return (
+    <>
+      {!collapsed && (
+        <div className="p-3 pl-6 mb-2 flex items-center w-full">
+          <p className="">For you</p>
+          <Button
+            onClick={onCollapse}
+            className="h-auto p-2 ml-auto "
+            variant="ghost"
+          >
+            <ArrowLeftFromLine className="h-4 w-4" />
+          </Button>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default Toggle;
