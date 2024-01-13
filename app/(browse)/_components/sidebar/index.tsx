27e5990a702aae37c1d165447ea0a7f5a39d5c1a
brndalx <113251192+brandalx@users.noreply.github.com>
2024-01-12 20:20:46 -0500

    feat: recommended skeleton

--- app/(browse)/_components/sidebar/index.tsx
+++ app/(browse)/_components/sidebar/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Wrapper from "./wrapper";
 import Toggle, { ToggleSkeleton } from "./toggle";
-import Recommended from "./recommended";
+import Recommended, { RecommendedSkeleton } from "./recommended";
 import { getRecommended } from "@/lib/recommended-service";
 const Sidebar = async () => {
   const recommended = await getRecommended();
@@ -19,6 +19,7 @@ export const SidebarSkeleton = () => {
   return (
     <aside className="fixed left-0 flex flex-col w-[70px] lg:w-60 h-full bg-background border-r border-[#2D2E35] z-50">
       <ToggleSkeleton />
+      <RecommendedSkeleton />
     </aside>
   );
 };
