d1b177c9e757887f69d23ec0912c95fc8460b677
brndalx <113251192+brandalx@users.noreply.github.com>
2024-01-12 19:47:27 -0500

    feat: UserAvatarProps

--- components/userAvatar.tsx
+++ components/userAvatar.tsx
@@ -1,5 +1,17 @@
 import React from "react";
+import { cva, type VariantProps } from "class-variance-authority";
 
+import { cn } from "@/lib/utils";
+import { Skeleton } from "./ui/skeleton";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
+
+interface UserAvatarProps {
+  username: string;
+  imageUrl: string;
+  isLive?: boolean;
+  showBadge?: boolean;
+}
 const UserAvatar = () => {
   return <div>UserAvatar</div>;
 };
