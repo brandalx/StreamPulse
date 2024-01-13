423a3434102b8d9004a20cdd5db9d1e43aca5225
brndalx <113251192+brandalx@users.noreply.github.com>
2024-01-12 19:34:14 -0500

    feat: recommended initial ui, avatar comp init

--- app/(browse)/_components/sidebar/recommended.tsx
+++ app/(browse)/_components/sidebar/recommended.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { useSidebar } from "@/store/use-sidebar";
 import { User } from "@prisma/client";
 import React from "react";
 
@@ -5,8 +7,28 @@ interface RecommendedProps {
   data: User[];
 }
 
-const Recommended = ({ user }: RecommendedProps) => {
-  return <div>Recommended</div>;
+const Recommended = ({ data }: RecommendedProps) => {
+  const { collapsed } = useSidebar((state) => state);
+
+  const showLabel = !collapsed && data.length > 0;
+
+  return (
+    <div>
+      {showLabel && (
+        <div className="pl-6 mb-4">
+          <p className="text-sm text-muted-foreground"> Recommended</p>
+        </div>
+      )}
+
+      <ul className="space-y-2 px-2 ">
+        {data.map((user) => (
+          <div className="" key={user.id}>
+            {user.username}
+          </div>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Recommended;
