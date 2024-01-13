06896f2c27df97fd10d477b5a8b5314864f7eb48
brndalx <113251192+brandalx@users.noreply.github.com>
2024-01-12 19:42:10 -0500

    feat: initial ui for user item

--- app/(browse)/_components/sidebar/userItem.tsx
+++ app/(browse)/_components/sidebar/userItem.tsx
@@ -1,11 +1,45 @@
+"use client";
 import React from "react";
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { useSidebar } from "@/store/use-sidebar";
+import { Skeleton } from "@/components/ui/skeleton";
+import Link from "next/link";
 interface UserItemProps {
   username: string;
   imageUrl: string;
   isLive?: boolean;
 }
 const UserItem = ({ username, imageUrl, isLive }: UserItemProps) => {
-  return <div>UserItem</div>;
+  const pathname = usePathname();
+  const { collapsed } = useSidebar((state) => state);
+
+  const href = `/${username}`;
+  const isActive = pathname === href;
+
+  return (
+    <Button
+      asChild
+      variant="ghost"
+      className={cn(
+        "w-full h-12 ",
+        collapsed ? "justify-center " : "justify-start",
+        isActive && "bg-accent"
+      )}
+    >
+      <Link href={href} className="">
+        <div
+          className={cn(
+            "flex items-center w-full gap-x-4 ",
+            collapsed && "justify-center"
+          )}
+        >
+          user avatar
+        </div>
+      </Link>
+    </Button>
+  );
 };
 
 export default UserItem;
