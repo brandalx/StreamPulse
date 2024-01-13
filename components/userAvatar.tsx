57a0263f59b16f514684387b1ef15a9635ccefcb
brndalx <113251192+brandalx@users.noreply.github.com>
2024-01-12 19:52:17 -0500

    feat: avatarSizes

--- components/userAvatar.tsx
+++ components/userAvatar.tsx
@@ -6,13 +6,30 @@ import { Skeleton } from "./ui/skeleton";
 
 import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
 
-interface UserAvatarProps {
+const avatarSizes = cva("", {
+  variants: {
+    size: {
+      default: "h-8 w-8  ",
+      lg: "h-14 w-14",
+    },
+    defaultVariants: {
+      size: "default",
+    },
+  },
+});
+
+interface UserAvatarProps extends VariantProps<typeof avatarSizes> {
   username: string;
   imageUrl: string;
   isLive?: boolean;
   showBadge?: boolean;
 }
-const UserAvatar = () => {
+const UserAvatar = ({
+  username,
+  imageUrl,
+  isLive,
+  showBadge,
+}: UserAvatarProps) => {
   return <div>UserAvatar</div>;
 };
 
