1490df55d107bd7f73622dfa950adb1e7c2f5c19
brndalx <113251192+brandalx@users.noreply.github.com>
2024-01-12 19:23:55 -0500

    feat: Recommended comp init

--- app/(browse)/_components/sidebar/index.tsx
+++ app/(browse)/_components/sidebar/index.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Wrapper from "./wrapper";
 import Toggle from "./toggle";
+import Recommended from "./recommended";
 
 const Sidebar = () => {
   return (
     <Wrapper>
       <Toggle />
+      <div className="space-y-4 pt-4 lg:pt-0">
+        <Recommended />
+      </div>
     </Wrapper>
   );
 };
