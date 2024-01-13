907dbe0c68f580cf5f97a161b05ff3613c0f11a2
brndalx <113251192+brandalx@users.noreply.github.com>
2024-01-12 19:15:45 -0500

    fix: return user for auth service

--- lib/auth-service.tsx
+++ lib/auth-service.tsx
@@ -11,4 +11,9 @@ export const getSelf = async () => {
   const user = await db.user.findUnique({
     where: { externalUserId: self.id },
   });
+
+  if (!user) {
+    throw new Error("User not found");
+  }
+  return user;
 };
